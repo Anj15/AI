91ff2a42eda636b9896ae8e6d8b48f9361fc0d80
anjali <33088798+Anj15@users.noreply.github.com>
2018-03-09 11:29:38 +0530

    Update towerofhanoi.pl

--- towerofhanoi.pl
+++ towerofhanoi.pl
@@ -1,4 +1,12 @@
-hanoi(N):- move(N,l,c,r).
-move(0,_,_,_).
-move(N,A,B,C):-M is N-1,move(M,A,C,B),inform(A,B),move(M,C,B,A).
-inform(X,Y):-write([move,a,disk,from,the,X,pole,to,Y,pole]),nl.
+move(1,X,Y,_) :-  
+    write('Move top disk from '), 
+    write(X), 
+    write(' to '), 
+    write(Y), 
+    nl. 
+move(N,X,Y,Z) :- 
+    N>1, 
+    M is N-1, 
+    move(M,X,Z,Y), 
+    move(1,X,Y,_), 
+    move(M,Z,Y,X).
